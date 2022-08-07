a2472ee401ed37fc731211d2c5f00e3fd3f8e170
owen corrigan <owencorrigan76@gmail.com>
2022-08-07 08:20:55 +0100

    README updated

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -134,3 +134,21 @@ export const getVideo = async () => {
     throw error;
   }
 };
+
+export const getPersonn = (args) => {
+  // console.log(args)
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/{person_id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
